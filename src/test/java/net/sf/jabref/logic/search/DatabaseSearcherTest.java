d19a8276f281ecca6b5f3684b193f0bb960e9308
Antonio Ribeiro <tonhogeek@gmail.com>
2016-05-03 16:00:15 -0300

    Adicionado testes para DatabaseSearcher, teste com base mínima

--- src/test/java/net/sf/jabref/logic/search/DatabaseSearcherTest.java
+++ src/test/java/net/sf/jabref/logic/search/DatabaseSearcherTest.java
@@ -5,11 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import net.sf.jabref.model.database.BibDatabase;
+import net.sf.jabref.model.entry.BibEntry;
+import net.sf.jabref.model.entry.BibtexEntryTypes;
 
 public class DatabaseSearcherTest {
 
     @Test
-    public void testGetDatabasefromMatches() {
+    public void testGetDatabasefromMatchesEmptyDatabase() {
         BibDatabase database = new BibDatabase();
         SearchQuery query = new SearchQuery("asdf", true, true);
 
@@ -19,10 +21,38 @@ public class DatabaseSearcherTest {
     }
 
     @Test
-    public void testGetDatabasefromMatchesWithInvalidQuery() {
+    public void testGetDatabasefromMatchesEmptyDatabaseWithInvalidQuery() {
         BibDatabase database = new BibDatabase();
         SearchQuery query = new SearchQuery("asdf[", true, true);
 
+        DatabaseSearcher databaseSearcher = new DatabaseSearcher(query, database);
+
+        assertTrue(databaseSearcher.getDatabasefromMatches().getEntries().isEmpty());
+    }
+
+    @Test
+    public void testGetDatabasefromMatches() {
+        BibDatabase database = new BibDatabase();
+        BibEntry entry = new BibEntry();
+        entry.setType(BibtexEntryTypes.INCOLLECTION);
+        entry.setField("author", "tonho");
+        database.insertEntry(entry);
+
+        SearchQuery query = new SearchQuery("tonho", true, true);
+        DatabaseSearcher databaseSearcher = new DatabaseSearcher(query, database);
+
+        assertFalse(databaseSearcher.getDatabasefromMatches().getEntries().isEmpty());
+    }
+
+    @Test
+    public void testGetDataBaseFromMatchesWithoutMatch() {
+        BibDatabase database = new BibDatabase();
+        BibEntry entry = new BibEntry();
+        entry.setType(BibtexEntryTypes.INCOLLECTION);
+        entry.setField("author", "tonho");
+        database.insertEntry(entry);
+
+        SearchQuery query = new SearchQuery("asdf", true, true);
         DatabaseSearcher databaseSearcher = new DatabaseSearcher(query, database);
 
         assertTrue(databaseSearcher.getDatabasefromMatches().getEntries().isEmpty());
