e0ddc8aedc00439230cc45e93455178384e98914
Breno Cruz <brenocruzb@gmail.com>
2016-05-05 01:51:07 -0300

    Construida a classe de teste RemoveToGroupActionTest
    Classe contruida com o objetivo de testar a remo;áo de entrys nos grupos
    das referëncias.

--- src/main/java/net/sf/jabref/groups/RemoveFromGroupAction.java
+++ src/main/java/net/sf/jabref/groups/RemoveFromGroupAction.java
@@ -66,7 +66,7 @@ public class RemoveFromGroupAction extends AbstractAction {
             return; // no changed made
         }
 
-        mPanel.undoManager.addEdit(undo);
+        mPanel.undoManagerAddEdit(undo);
         mPanel.markBaseChanged();
         mPanel.updateEntryEditorIfShowing();
         mPanel.getGroupSelector().valueChanged(null);
