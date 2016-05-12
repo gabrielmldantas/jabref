0055eac9ef36206b50cc1e9a62c57a2e038c05fb
Junior <juniorcaemj@gmail.com>
2016-05-11 21:41:59 -0300

    - Add métodos base de teste

--- src/test/java/br/ufs/ds3/group/gui/RemoveGroupGuiTest.java
+++ src/test/java/br/ufs/ds3/group/gui/RemoveGroupGuiTest.java
@@ -1,6 +1,36 @@
 package br.ufs.ds3.group.gui;
 
+import javax.swing.JFrame;
 
-public class RemoveGroupGuiTest {
+import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.assertj.swing.finder.WindowFinder;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.assertj.swing.launcher.ApplicationLauncher;
+import org.junit.BeforeClass;
+
+import net.sf.jabref.JabRefMain;
+
+public class RemoveGroupGuiTest extends AssertJSwingJUnitTestCase {
+
+    private FrameFixture frameFixture;
+
+
+    @BeforeClass
+    public static void before() {
+        FailOnThreadViolationRepaintManager.uninstall();
+    }
+
+    @Override
+    protected void onSetUp() {
+        ApplicationLauncher.application(JabRefMain.class).start();
+        frameFixture = WindowFinder.findFrame(JFrame.class).using(robot());
+    }
+
+    @Override
+    protected void onTearDown() {
+        frameFixture.close();
+        frameFixture = null;
+    }
 
 }
